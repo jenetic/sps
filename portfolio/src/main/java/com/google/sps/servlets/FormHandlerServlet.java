6ea55070598bebe6a61909af8c04ee92b4a2b3c8
Jenny <jznnylam@gmail.com>
2022-06-20 06:05:51 +0000

    Added datastore to portfolio

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -1,10 +1,17 @@
 package com.google.sps.servlets;
 
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.KeyFactory;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
 
 @WebServlet("/form-handler")
 public class FormHandlerServlet extends HttpServlet {
@@ -13,13 +20,19 @@ public class FormHandlerServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
     // Get the value entered in the form.
-    String textValue = request.getParameter("text-input");
+    // String textValue = request.getParameter("text-input");
+    
+    String textValue = Jsoup.clean(request.getParameter("text-input"), Whitelist.none());
 
-    // Print the value so you can see it in the server logs.
-    System.out.println("You submitted: " + textValue);
-
-    // Write the value to the response so the user can see it.
-    response.getWriter().println("You submitted: " + textValue);
+    // Create instance of Datastore
+    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    
+    KeyFactory keyFactory = datastore.newKeyFactory().setKind("Messages");    
+    FullEntity taskEntity =
+    Entity.newBuilder(keyFactory.newKey())
+        .set("title", textValue)
+        .build();
+    datastore.put(taskEntity);
 
     // Redirect back to website
     response.sendRedirect("/");
